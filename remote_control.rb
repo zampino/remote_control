5407ac1c7f99793305cdebd840f06d7964bcb489
Andrea Amantini <lo.zampino@gmail.com>
2014-11-04 00:47:20 +0100

    enable threaded mode -- enable logging

--- remote_control.rb
+++ remote_control.rb
@@ -9,30 +9,38 @@ class RemoteControl < Sinatra::Base
   set :server, :thin
   set :connections, {}
 
-  set :allow_origin, 'http://zampino.github.io'
+  set :allow_origin, lambda { environment == 'production' ?     'http://zampino.github.io' : 'http://localhost:4000' }
   set :allow_methods, [:get, :post, :options]
   set :allow_credentials, false
   set :max_age, "1728000"
   set :expose_headers, ['Content-Type']
   set :allow_headers, ['*', 'X-Requested-With', 'X-HTTP-Method-Override', 'Content-Type', 'Cache-Control', 'Accept', 'AUTHORIZATION']
+  set :threaded, true
 
   configure do
     enable :cross_origin
+    enable :logging
   end
 
   get '/' do
-    "ok, connections: #{settings.connections.keys.join('<br />')}"
+    "env: #{settings.environment}<br />threaded: #{settings.threaded}<br />connections: <br />#{settings.connections.keys.join('<br />')}"
   end
 
   get '/connections/:key', provides: 'text/event-stream' do |key|
     stream(:keep_open) { |out|
-      settings.connections.store(key, Connection.new(key, out))
-      out.errback { settings.connections.delete(key) }
+      settings.connections.store(key, Connection.new(key, out, logger))
+      out.callback {
+        logger.info "[CLOSE]: #{key}"
+        settings.connections.delete(key)
+      }
+      out.errback {
+        logger.error "[ERROR]: #{key}"
+        settings.connections.delete(key)
+      }
     }
   end
 
   options '/connections/:key' do
-    puts '//////', params
     halt 200
   end
 
@@ -43,14 +51,15 @@ class RemoteControl < Sinatra::Base
   end
 
   class Connection
-    def initialize(key, out)
+    def initialize(key, out, logger)
       @key = key
       @out = out
+      @logger = logger
       handshake
     end
 
     def handshake
-      puts '[HANDSHAKE]:', @key
+      @logger.info "[HANDSHAKE]: #{@key}"
       @out << "retry: 1000\nid: #{@key}\nevent: handshake\ndata: connected #{@key}\n\n"
     end
 
@@ -59,7 +68,7 @@ class RemoteControl < Sinatra::Base
     end
 
     def write(data)
-      puts '[DATA]:', data
+      @logger.info "[DATA]: #{data}"
       @out << data(data)
     end
 
